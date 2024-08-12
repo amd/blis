b4612a0ec93bf2330468608ec471f9313c44d258
sireesha.sanga <sireesha.sanga@amd.com>
2024-08-12 13:32:39 +0000

    AOCL LPGEMM Enhancements
    - Improved performance of all existing API's
    
    - Improved smart threading for all API's16|s32
    
    - Added new LPGEMM API's
       aocl_gemm_S8S8S32o<s32|S8>,
       aocl_gemm_s8s8s16o<s16|s8>,
       aocl_gemm_bf16s4f32o<f32|bf16>
    
    - Added TransA, TransB support of all AVX512 API's16|s32
    
    - Added JIT-baseed micro-kernel for bf16 to support
    gcc compiler version less than 11.2
    
    - Developed optimal LPGEMV kernels for m=1 or n=1
    inputs instead of GEMM path.
    
    - Added below support for SWISH, mat_add, mat_mul
    post-ops and multiple same post-ops support feature.
    
    - Fixed few functional and performance bugs.

--- addon/aocl_gemm/kernels/lpgemm_kernels.h
+++ addon/aocl_gemm/kernels/lpgemm_kernels.h
@@ -4,7 +4,7 @@
    An object-based framework for developing high-performance BLAS-like
    libraries.
 
-   Copyright (C) 2022 - 2023, Advanced Micro Devices, Inc. All rights reserved.
+   Copyright (C) 2022 - 2024, Advanced Micro Devices, Inc. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -38,6 +38,13 @@
 #include "lpgemm_post_ops.h"
 #include "aocl_bf16_type.h"
 
+// Disable BF16 kernel in cases where compilers support other avx 512
+// features except BF16 ISA.
+#if ( defined( BLIS_GCC ) && ( ( __GNUC__ < 11 ) || \
+	  ( ( __GNUC__ == 11 ) && ( __GNUC_MINOR__ < 2 ) ) ) )
+#define LPGEMM_BF16_JIT
+#endif
+
 typedef void (*lpgemm_m_fringe_f32_ker_ft)
     (
        const dim_t         k0,
@@ -52,7 +59,7 @@ typedef void (*lpgemm_m_fringe_f32_ker_ft)
        const float         alpha,
        const float         beta,
        lpgemm_post_op*     post_ops_list,
-       lpgemm_post_op_attr post_ops_attr 
+       lpgemm_post_op_attr post_ops_attr
     );
 
 #define LPGEMM_MAIN_KERN(A_type,B_type,C_type,LP_SFX) \
@@ -365,5 +372,67 @@ LPGEMM_MN_LT_NR0_FRINGE_KERN(int8_t,int8_t,int32_t,s8s8s32os32_1xlt16);
 LPGEMM_MN_LT_NR0_FRINGE_KERN(int8_t,int8_t,int16_t,s8s8s16o16_4xlt16);
 LPGEMM_MN_LT_NR0_FRINGE_KERN(int8_t,int8_t,int16_t,s8s8s16o16_2xlt16);
 LPGEMM_MN_LT_NR0_FRINGE_KERN(int8_t,int8_t,int16_t,s8s8s16o16_1xlt16);
+
+#define LPGEMV_M_EQ1_KERN(A_type,B_type,C_type,LP_SFX) \
+void lpgemv_m_one_ ## LP_SFX \
+( \
+	const dim_t           n0, \
+	const dim_t           k, \
+	const A_type          *a, \
+	const dim_t           rs_a, \
+	const dim_t           cs_a, \
+	const AOCL_MEMORY_TAG mtag_a, \
+	const B_type          *b, \
+	dim_t                 rs_b, \
+	const dim_t           cs_b, \
+	const AOCL_MEMORY_TAG mtag_b, \
+	C_type                *c, \
+	const dim_t           rs_c, \
+	const dim_t           cs_c, \
+	const C_type          alpha, \
+	const C_type          beta, \
+	dim_t                 NR, \
+	const dim_t           KC, \
+	const dim_t           n_sub_updated, \
+	const dim_t           jc_cur_loop_rem, \
+	lpgemm_post_op        *post_op, \
+	lpgemm_post_op_attr   *post_op_attr \
+  ) \
+
+LPGEMV_M_EQ1_KERN(float, float, float,f32f32f32of32);
+LPGEMV_M_EQ1_KERN(bfloat16,bfloat16,float,bf16bf16f32of32);
+LPGEMV_M_EQ1_KERN(uint8_t,int8_t,int32_t,u8s8s32os32);
+LPGEMV_M_EQ1_KERN(int8_t,int8_t,int32_t,s8s8s32os32);
+
+#define LPGEMV_N_EQ1_KERN(A_type,B_type,C_type,LP_SFX) \
+void lpgemv_n_one_ ## LP_SFX \
+( \
+	const dim_t           m0, \
+	const dim_t           k, \
+	const A_type          *a, \
+	const dim_t           rs_a, \
+	const dim_t           cs_a, \
+	const AOCL_MEMORY_TAG mtag_a, \
+	const B_type          *b, \
+	const dim_t           rs_b, \
+	const dim_t           cs_b, \
+	const AOCL_MEMORY_TAG mtag_b, \
+	C_type                *c, \
+	const dim_t           rs_c, \
+	const dim_t           cs_c, \
+	const C_type          alpha, \
+	const C_type          beta, \
+	const dim_t           MR, \
+	const dim_t           KC, \
+	lpgemm_post_op        *post_op, \
+	lpgemm_post_op_attr   *post_op_attr \
+) \
+
+LPGEMV_N_EQ1_KERN(float, float, float,f32f32f32of32);
+LPGEMV_N_EQ1_KERN(bfloat16, bfloat16, float,bf16bf16f32of32);
+LPGEMV_N_EQ1_KERN(uint8_t,int8_t,int32_t,u8s8s32os32);
+LPGEMV_N_EQ1_KERN(uint8_t,int8_t,int16_t,u8s8s16os16);
+LPGEMV_N_EQ1_KERN(int8_t,int8_t,int32_t,s8s8s32os32);
+LPGEMV_N_EQ1_KERN(int8_t,int8_t,int16_t,s8s8s16os16);
 
 #endif //BLIS_LPGEMM_KERN_H
